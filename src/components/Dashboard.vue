971962e6217418fd9d17105fd7c7f9fa58fe5bea
wiley68 <ilko.iv@gmail.com>
2022-02-25 09:43:14 +0200

    dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,12 +2,7 @@
   <main>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
       <WelcomeBanner />
-      <div class="sm:flex sm:justify-between sm:items-center mb-8">
-        <DashboardAvatars />
-        <div class="grid grid-flow-col sm:auto-cols-max justify-end gap-2">
-          <FilterButton align="right" />
-        </div>
-      </div>
+      <div class="sm:flex sm:justify-between sm:items-center mb-8"></div>
     </div>
   </main>
 </template>
@@ -15,16 +10,12 @@
 <script>
 import { inject } from 'vue'
 import WelcomeBanner from './includes/dashboard/WelcomeBanner.vue'
-import DashboardAvatars from './includes/dashboard/DashboardAvatars.vue'
-import FilterButton from './includes/components/DropdownFilter.vue'
 
 export default {
   name: 'Dashboard',
 
   components: {
     WelcomeBanner,
-    DashboardAvatars,
-    FilterButton,
   },
 
   setup() {
