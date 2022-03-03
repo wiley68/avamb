23dee0b44288d2c2f3f47731a609c1b6fe9b4baf
wiley68 <ilko.iv@gmail.com>
2022-03-03 11:07:36 +0200

    Update Dashboard.vue

--- src/components/includes/dashboard/Dashboard.vue
+++ src/components/includes/dashboard/Dashboard.vue
@@ -9,32 +9,36 @@
       </p>
     </div>
   </div>
-  <div class="flex flex-row justify-center items-center p-4 sm:p-6 mb-2">
-    <Datepicker />
-    <button
-      class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
-      title="Покажи всички"
-      @click.stop="showAll()"
-    >
-      <svg class="w-6 h-6 fill-current text-blue-600" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8 15H16V17H8V15M8 18H16V20H8V18Z"
-        />
-      </svg>
-    </button>
-    <button
-      class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
-      title="Покажи активните"
-      @click.stop="showActive()"
-    >
-      <svg class="w-6 h-6 fill-current text-blue-600" viewBox="0 0 24 24">
-        <path
-          fill="currentColor"
-          d="M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9Z"
-        />
-      </svg>
-    </button>
+  <div
+    class="flex flex-wrap justify-center items-center space-y-2 p-4 sm:p-6 mb-2"
+  >
+    <Datepicker class="mt-2" />
+    <div>
+      <button
+        class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
+        title="Покажи всички"
+        @click.stop="showAll()"
+      >
+        <svg class="w-6 h-6 fill-current text-blue-600" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9M8 12H16V14H8V12M8 15H16V17H8V15M8 18H16V20H8V18Z"
+          />
+        </svg>
+      </button>
+      <button
+        class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
+        title="Покажи активните"
+        @click.stop="showActive()"
+      >
+        <svg class="w-6 h-6 fill-current text-blue-600" viewBox="0 0 24 24">
+          <path
+            fill="currentColor"
+            d="M3 4H21V8H19V20H17V8H7V20H5V8H3V4M8 9H16V11H8V9Z"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
   <ul class="mb-6">
     <li v-for="offer in getOffers()" :key="offer.id" class="-mx-2 mb-1">
