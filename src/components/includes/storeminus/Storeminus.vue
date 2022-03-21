ea85dfeb1a6f879ea43c53a463390768314c8f3f
Ilko Ivanov <ilko.iv@gmail.com>
2022-03-21 19:41:42 +0200

    storeminus

--- src/components/includes/storeminus/Storeminus.vue
+++ src/components/includes/storeminus/Storeminus.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex flex-col py-3 w-full">
     <div class="flex flex-row justify-between items-center mb-1">
+      <span class="p-1 w-full font-medium"
+        >Експедиционна бележка №:
+        <span class="text-red-600">{{ storeminus.id }}</span></span
+      >
+    </div>
+    <div class="flex flex-row justify-between items-center mb-1">
       <select
         class="w-full text-sm border rounded-sm border-gray-100 p-1 bg-blue-300"
         v-model="storeminus.contragent_id"
@@ -17,11 +23,19 @@
     </div>
     <div class="flex flex-row justify-between items-center mb-1">
       <span class="p-1 text-sm w-3/12">Приключване</span>
-      <input type="date" v-model="storeminus.dateon" />
+      <input
+        type="date"
+        v-model="storeminus.dateon"
+        class="w-7/12 text-sm border rounded-sm border-gray-100 p-1 bg-blue-300"
+      />
     </div>
     <div class="flex flex-row justify-between items-center mb-1">
       <span class="p-1 text-sm w-3/12">Напомняне</span>
-      <input type="text" v-model="storeminus.parvicen" />
+      <input
+        type="text"
+        v-model="storeminus.parvicen"
+        class="w-7/12 text-sm border rounded-sm border-gray-100 p-1 bg-blue-300"
+      />
     </div>
   </div>
 </template>
