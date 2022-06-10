2303c8d18d30c762bd83b0b98549b285d54835f8
wiley68 <ilko.iv@gmail.com>
2022-06-10 13:19:18 +0300

    M.63

--- src/components/includes/dashboard/Dashboard.vue
+++ src/components/includes/dashboard/Dashboard.vue
@@ -38,6 +38,21 @@
           />
         </svg>
       </button>
+      <button
+        class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
+        title="Създай оферта"
+        @click.stop="createOffer()"
+      >
+        <svg
+          class="w-6 h-6 fill-current text-blue-600 mr-1 shrink-0"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z"
+          />
+        </svg>
+      </button>
     </div>
   </div>
   <ul class="mb-6">
@@ -136,6 +151,8 @@ export default {
       store.methods.closeDashboardSidebar()
     }
 
+    const createOffer = () => {}
+
     return {
       store,
       changeDashboardOffer,
@@ -144,6 +161,7 @@ export default {
       showActive,
       getOffers,
       openOffer,
+      createOffer,
     }
   },
 }
