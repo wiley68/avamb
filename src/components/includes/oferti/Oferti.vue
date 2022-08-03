74502efa1df73116e9013488ec2dd2182e050996
wiley68 <ilko.iv@gmail.com>
2022-08-03 08:39:59 +0300

    offer

--- src/components/includes/oferti/Oferti.vue
+++ src/components/includes/oferti/Oferti.vue
@@ -1,64 +1,57 @@
 <template>
-  <div class="relative bg-sky-700 p-2 rounded overflow-hidden mb-2">
-    <div class="relative">
-      <h1 class="text-xl text-white font-medium">
-        AVAMB-LOGICIEL - {{ typeUser() }}
-      </h1>
-      <p class="text-xl text-white">
-        {{ store.state.user.username }}
-      </p>
-    </div>
-  </div>
-  <div
-    class="flex flex-wrap justify-center items-center space-y-2 p-4 sm:p-6 mb-2"
-  >
-    <div>
-      <button
-        class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
-        title="Създай оферта"
-        @click.stop="createOffer()"
-      >
-        <svg
-          class="w-6 h-6 fill-current text-blue-600 mr-1 shrink-0"
-          viewBox="0 0 24 24"
+  <div class="sticky top-0 z-10 bg-white">
+    <div class="flex items-center bg-white border-b border-gray-200 px-5 h-16">
+      <div class="w-full flex items-center justify-between">
+        <button
+          class="p-1.5 shrink-0 rounded bg-white border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
+          title="Създай оферта"
+          @click.stop="createOffer()"
         >
-          <path
-            fill="currentColor"
-            d="M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z"
-          />
-        </svg>
-      </button>
+          <svg
+            class="w-6 h-6 fill-current text-blue-600 mr-1 shrink-0"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z"
+            />
+          </svg>
+        </button>
+        <div class="font-medium">Оферти</div>
+      </div>
     </div>
   </div>
-  <ul class="mb-6">
-    <li v-for="offer in getOffers()" :key="offer.id" class="-mx-2 mb-1">
-      <button
-        class="w-full p-2 rounded bg-sky-200 border border-sky-300"
-        @click.stop="editOffer(offer.idnomber)"
-      >
-        <div class="flex items-start justify-between">
-          <div class="flex-grow text-left">
-            <span class="text-sm font-medium text-gray-800 mr-1">{{
-              offer.client_name
-            }}</span>
+  <div class="px-5 py-4">
+    <ul class="mb-6">
+      <li v-for="offer in getOffers()" :key="offer.id" class="-mx-2 mb-1">
+        <button
+          class="w-full p-2 rounded bg-sky-200 border border-sky-300"
+          @click.stop="editOffer(offer.idnomber)"
+        >
+          <div class="flex items-start justify-between">
+            <div class="flex-grow text-left">
+              <span class="text-sm font-medium text-gray-800 mr-1">{{
+                offer.client_name
+              }}</span>
+            </div>
+            <div class="ml-1">
+              <span class="text-sm font-medium text-orange-700">{{
+                offer.idnomber
+              }}</span>
+            </div>
           </div>
-          <div class="ml-1">
-            <span class="text-sm font-medium text-orange-700">{{
-              offer.idnomber
-            }}</span>
+          <div class="flex items-start justify-between">
+            <div class="flex-grow text-left"></div>
+            <div class="ml-1">
+              <span class="text-sm font-medium text-red-700">{{
+                offer.allprice_after_to
+              }}</span>
+            </div>
           </div>
-        </div>
-        <div class="flex items-start justify-between">
-          <div class="flex-grow text-left"></div>
-          <div class="ml-1">
-            <span class="text-sm font-medium text-red-700">{{
-              offer.allprice_after_to
-            }}</span>
-          </div>
-        </div>
-      </button>
-    </li>
-  </ul>
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- src/components/includes/oferti/OfertiSidebar.vue
+++ src/components/includes/oferti/OfertiSidebar.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="dashboard-sidebar" class="absolute z-20 top-0 bottom-0 w-full">
+  <div
+    id="dashboard-sidebar"
+    class="absolute z-20 top-0 bottom-0 w-full md:w-auto md:static md:top-auto md:bottom-auto -mr-px md:translate-x-0 transform transition-transform duration-200 ease-in-out"
+  >
     <div
-      class="sticky top-16 bg-white overflow-x-hidden overflow-y-auto no-scrollbar shrink-0 border-r border-gray-200 md:w-72 xl:w-80 h-[calc(100vh-64px)]"
+      class="sticky top-16 overflow-x-hidden overflow-y-auto no-scrollbar shrink-0 border-r border-gray-200 md:w-72 xl:w-80 h-[calc(100vh-64px)]"
     >
       <div>
-        <div class="px-5 py-4"><Oferti /></div>
+        <Oferti />
       </div>
     </div>
   </div>
