51dd196638750b404c2ab5c2ac3d4ccc47c2b690
wiley68 <ilko.iv@gmail.com>
2022-10-08 11:46:17 +0300

    Update Proformi.vue

--- src/components/includes/proformi/Proformi.vue
+++ src/components/includes/proformi/Proformi.vue
@@ -3,12 +3,19 @@
     <div class="flex items-center bg-white border-b border-gray-200 px-5 h-16">
       <div class="w-full flex items-center justify-between">
         <div class="">
-          <button v-if="store.state.current_oferti !== 0"
+          <button
+            v-if="store.state.current_oferti !== 0"
             class="flex flex-row justify-center items-center p-1.5 shrink-0 rounded border border-gray-200 hover:border-gray-300 shadow-sm ml-2"
-            @click.stop="store.methods.createProforma(store.state.current_oferti)">
-            <svg class="w-6 h-6 fill-current text-blue-600 mr-1 shrink-0" viewBox="0 0 24 24">
-              <path fill="currentColor"
-                d="M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z" />
+            @click.stop="store.methods.createProforma(store.state.current_oferti)"
+          >
+            <svg
+              class="w-6 h-6 fill-current text-blue-600 mr-1 shrink-0"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19,11H15V15H13V11H9V9H13V5H15V9H19M20,2H8A2,2 0 0,0 6,4V16A2,2 0 0,0 8,18H20A2,2 0 0,0 22,16V4A2,2 0 0,0 20,2M4,6H2V20A2,2 0 0,0 4,22H18V20H4V6Z"
+              />
             </svg>
           </button>
         </div>
@@ -19,38 +26,40 @@
   <div class="px-5 py-4">
     <ul class="mb-6">
       <li v-for="proforma in getProformi()" :key="proforma.id" class="-mx-2 mb-1">
-        <button class="flex flex-col w-full p-1 text-left bg-gray-100 rounded border border-gray-200"
-          @click.stop="editProforma(proforma.idnomber)">
+        <button
+          class="flex flex-col w-full p-1 text-left bg-gray-100 rounded border border-gray-200"
+          @click.stop="editProforma(proforma.idnomber)"
+        >
           <div class="flex w-full">
             <div class="flex-none">
               <span class="text-sm font-medium text-red-700 mr-1">{{
-              proforma.idnomber
-              }}</span><span class="text-gray-400">|</span>
+                proforma.idnomber
+              }}</span
+              ><span class="text-gray-400">|</span>
             </div>
             <div class="ml-1 flex-grow text-left">
-              <span class="text-sm font-medium text-gray-500">Оферта - {{ proforma.offer_idnomber }}</span>
+              <span class="text-sm font-medium text-gray-500"
+                >Оферта - {{ proforma.offer_idnomber }}</span
+              >
             </div>
             <div class="ml-1 text-right">
-              <span class="text-sm font-medium text-red-700">{{
-              proforma.price
-              }}</span>
+              <span class="text-sm font-medium text-red-700">{{ proforma.price }}</span>
             </div>
           </div>
           <div class="flex w-full">
             <div class="flex-none">
-              <span class="text-sm font-medium text-gray-500 mr-1">Частично плащане - {{ formatDateTime(proforma.dateon)
-              }}</span><span class="text-gray-400">|</span>
+              <span class="text-sm font-medium text-gray-500 mr-1"
+                >Частично плащане - {{ formatDateTime(proforma.dateon) }}</span
+              ><span class="text-gray-400">|</span>
             </div>
             <div class="flex-grow ml-1 text-right">
-              <span class="text-sm font-medium text-red-700">{{
-              proforma.avansi
-              }}</span>
+              <span class="text-sm font-medium text-red-700">{{ proforma.avansi }}</span>
             </div>
           </div>
           <div class="flex w-full">
             <div class="flex-grow">
               <span class="text-sm font-medium text-gray-500">{{
-              proforma.client_name
+                proforma.client_name
               }}</span>
             </div>
           </div>
@@ -61,42 +70,44 @@
 </template>
 
 <script>
-import { inject } from 'vue'
-import moment from 'moment'
+import { inject } from "vue";
+import moment from "moment";
 
 export default {
-  name: 'Proformi',
+  name: "Proformi",
 
   setup() {
-    const store = inject('store')
+    const store = inject("store");
 
     const getProformi = () => {
       if (store.state.current_oferti !== 0) {
-        store.state.proformi_temp = store.state.proformi.filter(element => element.offer_idnomber == store.state.current_oferti)
+        return store.state.proformi.filter(
+          (element) => element.offer_idnomber == store.state.current_oferti
+        );
       } else {
-        store.state.proformi_temp = store.state.proformi
+        return store.state.proformi;
       }
-      return store.state.proformi_temp
-    }
+    };
 
     const editProforma = (proforma_idnomber) => {
-      store.state.current_proformi = proforma_idnomber
+      store.state.current_proformi = proforma_idnomber;
       setTimeout(() => {
-        store.methods.changePage('Proforma')
-      }, 500)
-    }
+        store.methods.changePage("Proforma");
+      }, 500);
+    };
 
     const formatDateTime = (value) => {
       if (value) {
-        return moment(String(value)).format('DD.MM.YYYY')
+        return moment(String(value)).format("DD.MM.YYYY");
       }
-    }
+    };
 
     return {
       store,
       getProformi,
-      editProforma, formatDateTime
-    }
+      editProforma,
+      formatDateTime,
+    };
   },
-}
+};
 </script>
