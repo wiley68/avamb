1ef19585804e235194e91c91eda648ff7207fed6
wiley68 <ilko.iv@gmail.com>
2026-01-17 11:49:39 +0200

    sluz etapna

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -8,7 +8,7 @@
           <div>
             <div class="px-5 py-4">
               <ul class="mb-6">
-                <li v-if="store.state.user.role != 'sluz'" class="-mx-2 mb-1">
+                <li class="-mx-2 mb-1">
                   <button class="w-full p-2 rounded-lg border-2 border-blue-600"
                     @click.stop="store.methods.changePage('Dashboard')">
                     <div class="flex items-center">
